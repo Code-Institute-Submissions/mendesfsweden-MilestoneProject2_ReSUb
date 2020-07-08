e784dd8cb8031b46fdddfe25c91c5611637ad0c4
mendesfsweden <mendesf@hotmail.com>
2020-07-08 14:28:40 +0000

    created arrays of card and cover text elements and added click events

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,43 @@
-class AudioController {
+function start() {
+    let covers = Array.from(document.getElementsByClassName('cover-text'));
+    //array.from creates an array of the elements with a class name of "cover-text"
+    let cards = Array.from(document.getElementsByClassName('card'));
+    //array.from creates an array of the elements with a class name of "card"
+
+    covers.forEach(cover => {
+        cover.addEventListener('click', () => {
+            cover.classList.remove('visible');
+            //starGame()
+        })
+    })
+    /*loops through each element and on a click event it removes the class "visible" - 
+    this happens when the game starts, when the user loses or when the user wins - the clicking event 
+    also calls the startGame() function, initializing the game */
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            //flipcard(card)
+        })
+    })
+    //loops through each element and on a click event it calls the flipcard() function, fliping the card clicked - 
+    
+}
+
+start();
+
+
+
+class Audio {
     constructor() {
-        this.bgMusic = new Audio('');
-        this.winSound = new Audio('');
-        this.gameOverSound = new Audio('');
-        this.bgMusic.loop = true;
+        this.gameSong = new Audio('');
+        this.winAudio = new Audio('');
+        this.gameOverAudio = new Audio('');
     }
     startMusic() {
-        this.bgMusic.play();
+        this.gameSong.play();
     }
     stopMusic() {
-        this.bgMusic.pause();
-        this.bgMusic.currentTime = 0;
+        this.gameSong.pause();
+        this.gameSong.currentTime = 0;
     }
     victory() {
         this.stopMusic();
@@ -18,6 +45,6 @@ class AudioController {
     }
     gameOver() {
         this.stopMusic();
-        this.gameOverSound.play();
+        this.gameOverAudio.play();
     }
 }
