4b5414e7b04150ec27403a0404e88ec5813afb1b
mendesfsweden <mendesf@hotmail.com>
2020-07-21 23:36:18 +0000

    - changed some properties names -formating document -editing comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,146 +1,155 @@
 class Music {
     constructor() {
-        this.gameSong = new Audio('assets/audio/game-song.mp3');
+        this.gameSound = new Audio('assets/audio/game-song.mp3');
         this.winSound = new Audio('assets/audio/win-sound.wav');
-        this.gameOverAudio = new Audio('assets/audio/game-over-sound.wav');
+        this.gameOverSound = new Audio('assets/audio/game-over-sound.wav');
     }
+
+    /**
+     * play game song
+     */
     startMusic() {
-        this.gameSong.play();
-        //play the game song
+        this.gameSound.play();
     }
+    /**
+     * Pauses the song and set 
+     */
     stopMusic() {
-        this.gameSong.pause();
-        this.gameSong.currentTime = 0;
-        /*pause() - pauses the song - there is no stop function for the Audio object in JS - 
-        then use the Audio currentTime property to set the song back to the begining/to 0 for when the user restarts the game*/
+        this.gameSound.pause();
+        // currentTime property sets the song back to the begining
+        this.gameSound.currentTime = 0;
     }
-    win() {
+
+    playWinSound() {
         this.stopMusic();
         this.winSound.play();
-        //once the user wins,first the stopMusic() is called to pause the game song and only after the winSound() is called
     }
-    gameOver() {
+
+    playgameOverSound() {
         this.stopMusic();
-        this.gameOverAudio.play();
-        //once the user loses, first the stopMusic() is called to pause the game song and only after the gameOverAudio() is called
+        this.gameOverSound.play();
     }
-    mute(){
-        this.gameSong.muted = true;
+
+    mute() {
+        this.gameSound.muted = true;
         this.winSound.muted = true;
-        this.gameOverAudio.muted = true;
+        this.gameOverSound.muted = true;
     }
-    unmute(){
-        this.gameSong.muted = false;
+
+    unmute() {
+        this.gameSound.muted = false;
         this.winSound.muted = false;
-        this.gameOverAudio.muted = false;
+        this.gameOverSound.muted = false;
     }
 }
 
+/**
+ * MarioMatch contains all the game logic
+ */
 class MarioMatch {
     constructor(totalTime, cards) {
-        this.cards = cards;
         this.totalTime = totalTime;
-        /* above this comment are the properties of the object set from the constructor
-        under the comment are all the properties set dinamically*/
+        this.cards = cards;
         this.timeLeft = totalTime;
-        this.timer = document.getElementById('remaining-time');
-        this.scorer = document.getElementById('score');
+        this.time = document.getElementById('remaining-time');
+        this.score = document.getElementById('score');
         this.music = new Music();
     }
+
     startGame() {
         this.cardToCheck = null;
         this.totalScore = 1000;
         this.timeLeft = this.totalTime;
-        this.matchedCards = [];
-        /*all the cards matched throughout the game will be pushed into this array in order to check against the total cardsArray
+         /*all the cards matched throughout the game will be pushed into 
+        this array in order to check against the total cardsArray
         to see if there is a victory or not*/
-        this.noFlip = true;
+        this.matchedCards = [];
+        this.flipping = true;
+        /*setTimeout exists to create a smoother start of the game 
+        once the page is loaded or there is a game over/victory*/
         setTimeout(() => {
             this.music.startMusic();
             this.shuffleCards();
             this.countDown = this.startCountDown();
-            this.noFlip = false;
+            this.flipping = false;
         }, 500);
-        //setTimeout exists to create a smoother start of the game once the page is loaded or there is a game over/victory
-        this.hideCards();
-        this.timer.innerText = this.timeLeft;
-        this.scorer.innerText = this.totalScore;
+        this.cards.forEach(card => card.classList.remove('flip'));
         //reset both the score and time when a new game starts
-        
- 
-    }
-    hideCards() {
-        this.cards.forEach(card => {
-            card.classList.remove('flip');
-        });
+        this.time.innerText = this.timeLeft;
+        this.score.innerText = this.totalScore;
     }
 
-    flipCard(card) {
-        if(this.canFlipCard(card)) {
-            this.totalScore -=20;
-            this.scorer.innerText = this.totalScore;
-            card.classList.add('flip');
+    /**
+     * Flips the card if not flipped yet 
+     * @param {Element} card - the card to flip
+     */
+    flip(card) {
+        if (!this.canFlip(card)) return;
+        this.totalScore -= 20;
+        this.score.innerText = this.totalScore;
+        card.classList.add('flip');
 
-            if(this.cardToCheck)
-                this.checkForCardMatch(card);
-            else
-                this.cardToCheck = card;
-        }
+        if (this.cardToCheck) this.checkMatch(card);
+        else this.cardToCheck = card;
     }
-    checkForCardMatch(card) {
-        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
-            this.cardMatch(card, this.cardToCheck);
+
+    checkMatch(card) {
+        if (this.getType(card) === this.getType(this.cardToCheck))
+            this.cardsMatched(card, this.cardToCheck);
         else 
-            this.cardMisMatch(card, this.cardToCheck);
-
+            this.cardsMisMatched(card, this.cardToCheck);
         this.cardToCheck = null;
     }
-    cardMatch(card1, card2) {
+
+    cardsMatched(card1, card2) {
         this.matchedCards.push(card1);
         this.matchedCards.push(card2);
-        if(this.matchedCards.length === this.cards.length)
-            this.win(); 
+        if (this.matchedCards.length === this.cards.length)
+            this.win();
     }
-    cardMisMatch(card1, card2) {
-        this.noFlip = true;
+
+    cardsMisMatched(card1, card2) {
+        this.flipping = true;
         setTimeout(() => {
             card1.classList.remove('flip');
             card2.classList.remove('flip');
-            this.noFlip = false;
+            this.flipping = false;
         }, 750);
     }
 
-    getCardType(card) {
+    getType(card) {
         return card.getElementsByClassName('card-front')[0].src;
     }
+
     startCountDown() {
         return setInterval(() => {
             this.timeLeft--;
-            this.timer.innerText = this.timeLeft;
-            if(this.timeLeft === 0)
+            this.time.innerText = this.timeLeft;
+            if (this.timeLeft === 0)
                 this.gameOver();
         }, 1000);
         /* this makes the function be called every 1000 miliseconds (1 second), since the user has 60 sec to finish to game,
         once the countDown reaches 0 we check for game over and call the gameOver()*/
     }
+
     gameOver() {
-        clearInterval(this.countDown);
         //time stops counting down and it is reset to 60 (initial value) in the startGame()
-        this.music.gameOver();
+        clearInterval(this.countDown);
+        this.music.playgameOverSound();
         document.getElementById('game-over-text').classList.add('visible');
-        
-        
     }
+
     win() {
-        clearInterval(this.countDown);
         //time stops counting down and it is reset to 60 (initial value) in the startGame()
-        this.music.win();
+        clearInterval(this.countDown);
+        this.music.playWinSound();
         document.getElementById('win-text').classList.add('visible');
     }
- 
-     shuffleCards() {
-        for(let i = this.cards.length - 1; i > 0; i--) {
-            let randomIndex = Math.floor(Math.random() * (i+1));
+
+    shuffleCards() {
+        //loop through the array backwards due performance 
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const randomIndex = Math.floor(Math.random() * (i + 1));
             this.cards[randomIndex].style.order = i;
             this.cards[i].style.order = randomIndex;
         }
@@ -150,36 +159,43 @@ class MarioMatch {
          but instead shuffling the order of the cards the way it's being displayed*/
     }
 
-    canFlipCard(card) {
-        
-        return !this.noFlip && !this.matchedCards.includes(card) && card !== this.cardToCheck;
-        // allows or not the user to flip the card - all 3 statements have to be true in order to flip the card 
+    /**
+     * Verifies if card can be flipped
+     * @param {Element} card - the card to be checked
+     * @returns {boolean} tells if the card can be flipped or not
+     */
+    canFlip(card) {
+        /* Includes(card) call comes last due to effectiveness.
+        It verifies first the other two conditions that are faster
+        to check*/ 
+        return !(this.flipping || card === this.cardToCheck || this.matchedCards.includes(card));
     }
+
     restartGame() {
+        //time stops counting down and it is reset to 60 (initial value) in the startGame()
         clearInterval(this.countDown);
         this.music.stopMusic();
         this.startGame();
     }
-    muteMusic(){
+
+    muteMusic() {
         this.music.mute();
     }
-    unmuteMusic(){
+
+    unmuteMusic() {
         this.music.unmute();
     }
- 
 }
-
-
 
 function start() {
 
     const btns = Array.from(document.querySelectorAll('div.cover-text button'))
-   
+
     const cards = Array.from(document.getElementsByClassName('card'));
-        // creates an array of the elements with a class name of "card"
+    // creates an array of the elements with a class name of "card"
 
     const game = new MarioMatch(60, cards);
-        //creates instance of the MarioMatch object
+    //creates instance of the MarioMatch object
 
     btns.forEach(btn => {
         btn.addEventListener('click', () => {
@@ -187,16 +203,15 @@ function start() {
             btn.parentElement.classList.remove('visible')
         })
     })
-    
 
-        /*loops through each element and on a click event it removes the class "visible" - 
-        this happens when the game starts, when the user loses or when the user wins - the clicking event 
-        also calls the startGame() function, initializing the game */
+    /*loops through each element and on a click event it removes the class "visible" - 
+    this happens when the game starts, when the user loses or when the user wins - the clicking event 
+    also calls the startGame() function, initializing the game */
 
-    cards.forEach(card => card.addEventListener('click', MarioMatch.prototype.flipCard.bind(game, card)));
-        //loops through each element and on a click event it calls the flipcard() function, fliping the card clicked -
+    cards.forEach(card => card.addEventListener('click', MarioMatch.prototype.flip.bind(game, card)));
+    //loops through each element and on a click event it calls the flipcard() function, fliping the card clicked -
     document.getElementById('restart').addEventListener('click', MarioMatch.prototype.restartGame.bind(game));
-    
+
     const muteBtn = document.getElementById('mute');
 
     muteBtn.addEventListener('click', () => {
@@ -211,5 +226,4 @@ function start() {
     })
 }
 
-document.addEventListener('DOMContentLoaded', start) 
-
+document.addEventListener('DOMContentLoaded', start)
